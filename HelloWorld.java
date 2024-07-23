ba0447e19b9ea5eb2eb303807af3f2e43f89b2c8
AIRILCI <gwilci7@gmail.com>
2024-07-23 16:11:23 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+//main快捷生成 main 代码提示快捷键 option + ？
+//command + d 快捷删除一行
+//整理代码快捷键 command + shift + f
+//快速保存快捷键 command + s
+/*
+ * 这是多行注释
+ */
